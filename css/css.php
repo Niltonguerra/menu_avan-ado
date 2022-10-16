c933648b171bf743b0b6e55544969e2c649c2ba6
Nilton Dionisio Guerra <78931445+Niltonguerra@users.noreply.github.com>
2022-10-16 19:09:14 -0300

    finalização do menu avançado

--- css/css.php
+++ css/css.php
@@ -1,3 +1,4 @@
+<style>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500&display=swap');
 
 
@@ -144,3 +145,5 @@ pointer-events: auto;
     background-color: black;
     cursor: pointer;
 }
+
+</style>
--- js/pop-up.js
+++ js/pop-up.js
@@ -1,38 +1,22 @@
-const button = document.querySelector('.botao')
-const popup = document.querySelector('.popup-wrapper')
+document.addEventListener('click',(e) => {
+    const isDropdownBtn = e.target.classList.contains('dropdown');
 
-button.addEventListener('click', () => {
-    popup.style.display = 'block'
-})
+    if(!isDropdownBtn && e.target.closest('.navbar__item') != null) return;
 
-popup.addEventListener('click', () => {
-    const classNameOfClickedElement = event.target.classList[0]
-    const ClassNames = ['popup-close', 'popup-wrapper','popup-link']
-    const shouldClosePopup = ClassNames.some(className => className === classNameOfClickedElement)
+    let currentSubmenu
 
-    if(shouldClosePopup){
-        popup.style.display = 'none'
-    }
-})
-
-
-
-
-
-/*        
-        echo    '<div class="popup-wrapper">';
-        echo        '<div class="popup">';
-        echo            '<div class="popup-close">x</div>';
-        echo        '<div class="popup-content">';
-
-
-        echo                '<h2>teste do pop-up</h2>';
-        echo                '<p>Os testes devem ser parciais</p>';
-        echo        '<a  class="popup-link"href="#"> etc!!!</a>';
+    if(isDropdownBtn){
+        const item = e.target.closest('.navbar__item')
         
+        currentSubmenu = item.querySelector('.submenu')
 
-        echo "<a href=\"jogos.php\">VOLTAR</a>"; 
-        echo    '</div>';
-        echo'</div>';
-        echo'</div>';
-*/
+        currentSubmenu.classList.toggle('show')
+    }
+
+    document.querySelectorAll('.submenu.show').forEach(submenu => {
+        if(submenu == currentSubmenu)return
+
+        submenu.classList.remove('show');
+    });
+
+});
